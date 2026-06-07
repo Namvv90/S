cb71b862772b68e0a9554ce12469f21e7971dd15
Namvv90 <tttt25795@gmail.com>
2026-06-07 21:56:36 +0700

    Update Key.js

--- models/Key.js
+++ models/Key.js
@@ -4,7 +4,11 @@ const keySchema = new mongoose.Schema({
     key: String,
     used: { type: Boolean, default: false },
     discordId: String,
-    hwid: { type: String, default: "" }
+    hwid: { type: String, default: "" },
+    lastReset: {
+        type: Number,
+        default: 0
+    }
 });
 
 module.exports = mongoose.model("Key", keySchema);
